79dc2ea6a49ddbc952635375d2fe64476df6adc5
Mohammed Sayed <farsfhd123@gmail.com>
2023-09-25 14:38:38 +0300

    make some changes

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,32 +1,16 @@
-/**
- * How to use useState in React?
-    1- import useState function from react
-    2- pass the property or variable you want to change, in our case is useState(props.title),
-       useState function must called inside the component function which we want to reload,
-       this useState function returns an array => [the original value which we passed, a function which will update the original value],
-       we can assign the returned array using destructuring like this => const [title, setTitle] = useState(props.title),
-       so title variable will holds the origingal value and the value we want update which passed to for example to setTitle() function
-    3- call setTitle function inside clickHandler, which will called when the click event trigered
-    4- put onClick event on the button and assign to it the clickHandler
- */
-import React, {useState} from 'react';
+import React from 'react';
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  const clickHandler = () => {
-    setTitle("Updated!");
-  }
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,3 +1,14 @@
+/**
+ * How to use useState in React?
+    1- import useState function from react
+    2- pass the property or variable you want to change, in our case is useState(props.title),
+       useState function must called inside the component function which we want to reload,
+       this useState function returns an array => [the original value which we passed, a function which will update the original value],
+       we can assign the returned array using destructuring like this => const [title, setTitle] = useState(props.title),
+       so title variable will holds the origingal value and the value we want update which passed to for example to setTitle() function
+    3- call setTitle function inside clickHandler, which will called when the click event trigered
+    4- put onClick event on the button and assign to it the clickHandler
+ */
 import React, { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
@@ -6,13 +17,21 @@ import ExpensesFilter from './ExpensesFilter';
 
 const Expenses = (props) => {
   const [filteredYear, setFilteredYear] = useState("2020");
+
+  /* Derived State or Computed State : is a value or something changes accourding to any change in the state */
+  // let filterInfoText = "2019, 2021 & 2022";
+  // if(filteredYear === "2019") filterInfoText = "2020, 2021 & 2022";
+  // else if(filteredYear === "2020") filterInfoText = "2019, 2021 & 2022";
+  // else if(filteredYear === "2021") filterInfoText = "2019, 2020 & 2022";
+  // else filterInfoText = "2019, 2020 & 2021";
+
   const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-    console.log(filteredYear)
   }
   return (
       <Card className="expenses">
       <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler}/>
+      {/* <p>Data for years {filterInfoText} is hidden.</p> */}
       <ExpenseItem
         title={props.items[0].title}
         amount={props.items[0].amount}
